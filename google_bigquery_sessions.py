a7b855007af03966f45d4dd53ab60594be2763c2
nimitiitk <nimit.madbid@gmail.com>
2020-02-08 13:36:51 +0530

    initial commit BigQuery

---
+++ google_bigquery_sessions.py
@@ -0,0 +1,48 @@
+#Big Query
+
+from google.cloud import bigquery
+from google.oauth2 import service_account
+import pandas as pd
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+def getting_credentials():
+    credentials = service_account.Credentials.from_service_account_file('C:/Users/sachi/OneDrive/Desktop/Google_BigQuery/client_secret_358050000476-cn1c98do5mg0e43jvvvh3fgpklkdqlhs.apps.googleusercontent.com.json')
+    client = bigquery.Client(credentials = credentials)
+    return client
+
+def sql_query():
+    client = getting_credentials()
+    sql = '''
+          --strucutred query language; query
+
+            SELECT 
+                 TO_CHAR(DATE, 'DDD') AS week_of_the_year
+                ,SUM(sessions)
+            FROM 
+                 ga_union_sessions
+            WHERE 
+                 DATE BETWEEN '2019-01-01' AND '2020-01-01'
+            GROUP BY 
+                 1
+            ORDER BY 
+                 MIN(DATE)
+              ;
+          '''
+    df = client.query(sql).to_dataframe()
+    return df
+
+def plot_data():
+    df = plot_data()
+    fig, ax = plt.subplots(1, figsize = (30, 8))
+    ax.plot(df['week_of_the_year'], df['total_sessions'])
+    ax.set_xlabel('Week of the year')
+    ax.set_ylabel('Total Sessions')
+    ax.set_title('Week/Sessions', bbox={'facecolor':'0.8', 'pad':3})
+    plt.show()
+    
+def main():
+    plot_data()
+    
+if __name__ == '__main__':
+    main()
